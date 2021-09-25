754a88e94f1edbb92d1ee29cc1510189f36b222c
Juan Camilo Bastidas <juank2411@outlook.com>
2021-09-25 13:05:18 -0500

    route 1

--- kibhu/src/App.vue
+++ kibhu/src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <v-app>
     <v-app-bar app flat class="max-height">
-      <div class="d-flex align-center m-12">
+      <div class="d-flex align-center ma-12">
         <v-img
           contain
           lazy-src="..\src\assets\Logo Kibhu Para fondo claro -02.png"
           max-height="250"
-          max-width="150"
+          max-width="140"
           src="..\src\assets\Logo Kibhu Para fondo claro -02.png"
         ></v-img>
       </div>
 
       <v-spacer></v-spacer>
+
+      <div>
+        <a href="#" class="text-decoration-none black--text ma-12">Servicios</a>
+        <a href="#" class="text-decoration-none black--text ma-12">Quienes somos</a>
+        <a href="#" class="text-decoration-none black--text ma-12">Sobre Nosotros</a>
+        <a href="#" class="text-decoration-none black--text ma-12">Noticias</a>
+      </div>
 
       <v-btn
         class="ma-2"
@@ -27,14 +34,18 @@
 
     <v-main>
       <router-view />
+
     </v-main>
   </v-app>
 </template>
 
 <script>
+
+/*import LoginHome from '../components/loginhome'*/
+
 export default {
   name: "App",
-
+  
   data: () => ({
     //
   }),
