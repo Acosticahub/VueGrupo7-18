22e70905b52affa8f24f039e9bf74b7387ba612c
Juan Camilo Bastidas <juank2411@outlook.com>
2021-09-25 12:29:36 -0500

    header

--- kibhu/src/App.vue
+++ kibhu/src/App.vue
@@ -1,52 +1,39 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app>
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+          lazy-src="..\src\assets\Logo Kibhu Para fondo claro -02.png"
+          max-height="200"
+          max-width="130"
+          src="..\src\assets\Logo Kibhu Para fondo claro -02.png"
+        ></v-img>
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+        class="ma-2"
+        :loading="loading"
+        :disabled="loading"
+        color="#DAA520"
+        @click="loader = 'loading'"
       >
-        <span class="mr-2">Regístrate</span>
-        <v-icon>mdi-account-plus</v-icon>
+        <span class="mr-2 white--text">Regístrate</span>
+        <v-icon color="white">mdi-account-plus</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
     //
