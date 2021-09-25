a73f8784e3c67981f4380382c6d4ccf0dcf94d01
Juan Camilo Bastidas <juank2411@outlook.com>
2021-09-25 12:08:48 -0500

    cambios

--- kibhu/src/App.vue
+++ kibhu/src/App.vue
@@ -32,8 +32,8 @@
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">Regístrate</span>
+        <v-icon>mdi-account-plus</v-icon>
       </v-btn>
     </v-app-bar>
 
