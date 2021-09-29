9cccca20a4eab0d70c18f37fde4d557a75aab837
Juan Camilo Bastidas <juank2411@outlook.com>
2021-09-28 23:39:23 -0500

    Update App.vue

--- kibhu/src/App.vue
+++ kibhu/src/App.vue
@@ -1,13 +1,12 @@
 <template>
     <v-main>
-      
       <router-view />
     </v-main>
 
 </template>
 
 <script>
-/*import LoginHome from '../components/loginhome'*/
+
 export default {
   components: { },
   name: "App",
