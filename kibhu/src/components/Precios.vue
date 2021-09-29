627fb3ba7810f93c937be934dcf5122a1b3eb48e
Daniel Arias <82059705+TarAldarion1437@users.noreply.github.com>
2021-09-29 00:15:02 -0500

    precios

---
+++ kibhu/src/components/Precios.vue
@@ -0,0 +1,70 @@
+<template>
+<div class="pricing text-center">
+        <h3 class="display-4 fw-normal">Precios</h3>
+        <p class="fs-5 text-muted">Mira nuestras tarifas y escoge la que mejor se ajuste a tu plan de negocios</p>
+<div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+        <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3 bg-warning">
+            <h4 class="my-0 fw-normal">Prueba gratis</h4>
+            </div>
+            <div class="card-body">
+            <h1 class="card-title pricing-card-title">$0<small class="text-muted fw-light">/yr</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+                <li>Prueba de una semana de la aplicación</li>
+                <li>Guía de uso</li>
+                <li>Guarda tu inventario con nosotros y si decides seguir se actualizará</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-outline-secondary"><a href="loginvista.html">Registrate aquí</a></button>
+            </div>
+        </div>
+        </div>
+        <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3 bg-warning">
+            <h4 class="my-0 fw-normal">Plan Básico</h4>
+            </div>
+            <div class="card-body">
+            <h1 class="card-title pricing-card-title">$15<small class="text-muted fw-light">/yr</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+                <li>No tienes que empezar de cero tu inventario: lo actualizamos por ti</li>
+                <li>Almacenamiento en la nube. Siempre disponible para ti</li>
+                <li>Guía de uso</li>
+                <li>Asesorías personalizadas para darle mejor uso a tu inventario</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-outline-secondary">Únete ya</button>
+            </div>
+        </div>
+        </div>
+        <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm border-primary">
+            <div class="card-header py-3 text-white bg-dark">
+            <h4 class="my-0 fw-normal">Premium</h4>
+            </div>
+            <div class="card-body">
+            <h1 class="card-title pricing-card-title">$29<small class="text-muted fw-light">/yr</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+                <li>Une todas tus franquicias en un solo inventario</li>
+                <li>Almacenamiento en la nube. Siempre disponible para ti</li>
+                <li>Asesorías personalizadas para darle mejor uso a tu inventario</li>
+                <li>Reporte constante sobre el desempeño de tu inventario con la mejor información disponible para el crecimiento de tu empresa</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-outline-secondary">¿Te interesa?</button>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+ *{
+    padding: 10px;
+}
+</style>
