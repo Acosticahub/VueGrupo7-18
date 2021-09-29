79b5910f5cf70b7f8bebcc5f1b02088028265749
JuanAltamirano <mapuchi_12_2002@hotmail.com>
2021-09-28 19:44:51 -0500

    Update main.js

--- kibhu/src/main.js
+++ kibhu/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 Vue.config.productionTip = false
 
 new Vue({
