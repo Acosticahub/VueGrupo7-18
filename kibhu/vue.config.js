ff45a0078484d02904bf27677063373ecab93710
Juan Camilo Bastidas <juank2411@outlook.com>
2021-09-28 23:28:34 -0500

    .

--- kibhu/vue.config.js
+++ kibhu/vue.config.js
@@ -1,16 +1,6 @@
-const bootstrapSassAbstractsImports = require('vue-cli-plugin-bootstrap-vue/sassAbstractsImports.js')
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
   ]
 }
-	css: {
-		loaderOptions: {
-			sass: {
-				additionalData: bootstrapSassAbstractsImports.join('\n')
-			}
-			scss: {
-				additionalData: [...bootstrapSassAbstractsImports, ''].join(';\n')
-			}
-		}
-	}
