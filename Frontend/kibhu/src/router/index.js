1e74029a0290642e5834ddf150b5a54df944e6f8
Juan Camilo Bastidas <juank2411@outlook.com>
2021-10-05 21:28:54 -0500

    limpieza codigo

--- Frontend/kibhu/src/router/index.js
+++ Frontend/kibhu/src/router/index.js
@@ -1,111 +1,89 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+  import Vue from 'vue'
+  import VueRouter from 'vue-router'
+  import Home from '../views/Home.vue'
 
 
-Vue.use(VueRouter)
+  Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/appkibhu',
-    name: 'Appkibhu',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Appkibhu.vue')
-  },
-  {
-    path: '/HeaderHome',
-    name: 'HeaderHome',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../components/Web/HeaderHome.vue')
-  },
-  {
-    path: '/inicioapp',
-    name: 'InicioApp',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../components/Aplicacion/InicioApp.vue')
-  },
-  {
-    path: '/clientes',
-    name: 'Clientes',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../components/Aplicacion/Clientes/Clientes.vue')
-  },
-  {
-    path: '/newcliente',
-    name: 'NewCliente',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../components/Aplicacion/Clientes/NewCliente.vue')
-  },
-  {
-  path: '/productos',
-  name: 'Productos',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: () => import(/* webpackChunkName: "about" */ '../components/Aplicacion/Productos/Productos.vue')
-},
-{
-  path: '/newproducto',
-  name: 'NewProducto',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: () => import(/* webpackChunkName: "about" */ '../components/Aplicacion/Productos/NewProducto.vue')
-},
-{
-  path: '/usuarios',
-  name: 'Usuarios',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: () => import(/* webpackChunkName: "about" */ '../components/Aplicacion/Usuarios/Usuarios.vue')
-},
-{
-  path: '/newusuario',
-  name: 'NewUsuario',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: () => import(/* webpackChunkName: "about" */ '../components/Aplicacion/Usuarios/NewUsuario.vue')
-},
-{
-  path: '/proveedores',
-  name: 'Proveedores',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: () => import(/* webpackChunkName: "about" */ '../components/Aplicacion/Proveedores/Proveedores.vue')
-},
-{
-  path: '/newproveedores',
-  name: 'NewProveedores',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: () => import(/* webpackChunkName: "about" */ '../components/Aplicacion/Proveedores/NewProveedores.vue')
-},
+  const routes = [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/appkibhu',
+      name: 'Appkibhu',
 
-]
+      component: () => import( '../views/Appkibhu.vue')
+    },
+    {
+      path: '/HeaderHome',
+      name: 'HeaderHome',
+    
+      component: () => import( '../components/Web/HeaderHome.vue')
+    },
+    {
+      path: '/inicioapp',
+      name: 'InicioApp',
+    
+      component: () => import( '../components/Aplicacion/InicioApp.vue')
+    },
+    {
+      path: '/clientes',
+      name: 'Clientes',
+    
+      component: () => import( '../components/Aplicacion/Clientes/Clientes.vue')
+    },
+    {
+      path: '/newcliente',
+      name: 'NewCliente',
+    
+      component: () => import( '../components/Aplicacion/Clientes/NewCliente.vue')
+    },
+    {
+    path: '/productos',
+    name: 'Productos',
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+    component: () => import( '../components/Aplicacion/Productos/Productos.vue')
+  },
+  {
+    path: '/newproducto',
+    name: 'NewProducto',
 
-export default router
+    component: () => import( '../components/Aplicacion/Productos/NewProducto.vue')
+  },
+  {
+    path: '/usuarios',
+    name: 'Usuarios',
+
+    component: () => import( '../components/Aplicacion/Usuarios/Usuarios.vue')
+  },
+  {
+    path: '/newusuario',
+    name: 'NewUsuario',
+
+    component: () => import( '../components/Aplicacion/Usuarios/NewUsuario.vue')
+  },
+  {
+    path: '/proveedores',
+    name: 'Proveedores',
+
+    component: () => import( '../components/Aplicacion/Proveedores/Proveedores.vue')
+  },
+  {
+    path: '/newproveedores',
+    name: 'NewProveedores',
+
+    component: () => import( '../components/Aplicacion/Proveedores/NewProveedores.vue')
+  },
+
+  ]
+
+  const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+  })
+
+  export default router
