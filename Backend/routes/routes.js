750ff849a79d7365bcad2112df9a66178baa428c
Jhon 2man <dosman.diazjhon4166@gmail.com>
2021-10-07 00:24:58 -0500

    Revert "Cambios"
    This reverts commit 927fa3b40807734256a91ea6e8e38c8abb3abaaf.

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const clientController = require("../controllers/Client.ctrl")
-const userController = require("../controllers/User.ctrl")
 const router = express.Router();
 
 //client Routes
@@ -15,17 +14,5 @@ router.put("/clients/:identification",  clientController.update);
 router.delete("/clients/:identification",  clientController.delete);
 
 // Fin Clients route
-
-
-//user Routes
-router.get("/users", userController.getAll)
-
-router.get("/users/:code", userController.getByCode)
-
-
-router.post("/users",  userController.create);
-
-
-// Fin users route
 
 module.exports = router;
