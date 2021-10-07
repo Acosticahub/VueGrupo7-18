62a8eb65b64867b7c94081a176d28801838c4e8d
Jhon 2man <dosman.diazjhon4166@gmail.com>
2021-10-07 01:15:32 -0500

    Cambios 2.0
    Modelo y controlador de Usuarios
    ya no se cae la pagina tons a mimir

---
+++ Backend/models/user.model.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+
+//esto es para validar el correo
+require('mongoose-type-email');
+mongoose.SchemaTypes.Email.defaults.message = 'dirección de mail invalida'
+
+
+
+
+// MODELO DE USUARIOS
+const userSchema = mongoose.Schema({
+    name: {
+        first: {
+            type: String,
+            required: [true, 'Por favor digite un nombre']
+        },
+        last: {
+            type: String,
+            required: [true, 'Por favor digite un nombre']
+        }
+    },
+    mail: mongoose.SchemaTypes.Email,
+
+    contact: {
+        type: String,
+        maxlength: 10
+    },
+    password: {
+        type: String,
+        required: [true, "Digite su contraseña"],
+        minLength: 8,
+    },
+    username: {
+        type: String,
+        required: [true, "Digite su contraseña"],
+        match: /\w\d.\w/
+    },
+    identification: {
+        type: Number,
+        required: [true, 'Digite su número de identificación']
+    },
+    typeid: {
+        type: String,
+        required: [true, 'Seleccione un tipo de identificación válido'],
+        enum: ['CC', 'TI', 'Pasaporte', 'NIP']
+    },
+    role: {
+        type: String,
+        required: [true, 'Selecione un tipo de rol'],
+        enum: ['Administrador', 'Usuario'],
+    },
+});
+
+
+
+//Esto es para que me retorne el nombre completo
+userSchema.virtual('fullName').get(function () {
+    return this.name.first + '' + this.name.last;
+});
+
+//hasta aquí todo lo que es de usuarios
+
+
+
+//exportación
+
+module.exports = mongoose.model("User", userSchema);
