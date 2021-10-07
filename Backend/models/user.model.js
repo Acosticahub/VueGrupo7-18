927fa3b40807734256a91ea6e8e38c8abb3abaaf
Jhon 2man <dosman.diazjhon4166@gmail.com>
2021-10-07 00:06:45 -0500

    Cambios
    ojala no se caiga la pag

---
+++ Backend/models/user.model.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+//esto es para validar el correo
+require('mongoose-type-email');
+mongoose.SchemaTypes.Email.defaults.message = 'dirección de mail invalida'
+
+
+
+
+// MODELO DE USUARIOS
+const userSchema = mongoose.Schema({
+    name: {
+        first: {
+            type: String,
+            required: [true, 'Por favor digite un nombre']
+        },
+        last: {
+            type: String,
+            required: [true, 'Por favor digite un nombre']
+        }
+    },
+    mail: mongoose.SchemaTypes.Email,
+    contact: {
+        type: String,
+        maxlength: 10
+    },
+    identification: {
+        type: Number,
+        required: [true, 'Digite su número de identificación']
+    },
+    typeid: {
+        type: String,
+        required: [true, 'Seleccione un tipo de identificación válido'],
+        enum: ['CC', 'TI', 'Pasaporte', 'NIP']
+    },
+    date: { type: Date, default: Date.now}
+});
+
+
+//Esto es para que me retorne el nombre completo
+userSchema.virtual('fullName').get(function(){
+    return this.name.first + '' + this.name.last;
+});
+
+//hasta aquí todo lo que es de usuarios
+
+
+
+//exportación
+
+module.exports = mongoose.model("User", userSchema);
--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const clientController = require("../controllers/Client.ctrl")
+const userController = require("../controllers/User.ctrl")
 const router = express.Router();
 
 //client Routes
@@ -12,5 +13,17 @@ router.post("/clients",  clientController.create);
 
 
 // Fin Clients route
+
+
+//user Routes
+router.get("/users", userController.getAll)
+
+router.get("/users/:code", userController.getByCode)
+
+
+router.post("/users",  userController.create);
+
+
+// Fin users route
 
 module.exports = router;
