fa633ad34b411bbad1bf99267b8cb65aac5d27b3
Acosticahub <acostabuitrago0123@gmail.com>
2021-10-07 23:43:04 -0500

    Mostrar los products del Mongo
    Vamos a coronar muchachos!!

--- Frontend/kibhu/src/components/Aplicacion/Productos/Productos.vue
+++ Frontend/kibhu/src/components/Aplicacion/Productos/Productos.vue
@@ -1,49 +1,50 @@
 <template>
-<v-main>
-  <header-app />
-  <div>
-    <h1>Lista de Productos</h1>
-    <v-btn
-      class="mx-2"
-      dark
-      large
-      color="#dAA520"
-      elevation="2"
-      bottom
-      right
-      fixed
-      fab
-      to="/NewProducto"
-    >
-      <v-icon dark>mdi-tag-plus</v-icon>
-    </v-btn>
-    <v-container>
-      <v-col md="6">
-        <v-text-field label="Buscar">
-          <v-icon slot="append" color="red"> mdi-magnify </v-icon>
-        </v-text-field>
-      </v-col>
-
-      <v-row>
-        <v-col
-          v-for="product in products"
-          :key="product.id"
-          lg="3"
-          md="4"
-          sm="6"
-          xs="12"
-        >
-          <CatalogProducto :product="product" :active="active" />
+  <v-main>
+    <header-app />
+    <div>
+      <h1>Lista de Productos</h1>
+      <v-btn
+        class="mx-2"
+        dark
+        large
+        color="#dAA520"
+        elevation="2"
+        bottom
+        right
+        fixed
+        fab
+        to="/NewProducto"
+      >
+        <v-icon dark>mdi-tag-plus</v-icon>
+      </v-btn>
+      <v-container>
+        <v-col md="6">
+          <v-text-field label="Buscar">
+            <v-icon slot="append" color="red"> mdi-magnify </v-icon>
+          </v-text-field>
         </v-col>
-      </v-row>
-    </v-container>
-  </div>
-</v-main >
+
+        <v-row>
+          <v-col
+            v-for="product in products"
+            :key="product.reference"
+            lg="3"
+            md="4"
+            sm="6"
+            xs="12"
+          >
+            <CatalogProducto :product="product" :active="active" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+  </v-main>
 </template>
 
 <script>
+import { getAllProducts } from "../../../controllers/Product.controller";
 import CatalogProducto from "../../Aplicacion/Productos/CatalogProducto.vue";
-import HeaderApp from '../HeaderApp.vue';
+import HeaderApp from "../HeaderApp.vue";
 export default {
   components: {
     CatalogProducto,
@@ -56,10 +57,11 @@ export default {
     };
   },
   mounted() {
-    let productos = localStorage.products;
-    if (productos !== undefined && productos !== "") {
-      this.products = JSON.parse(productos);
-    }
+    getAllProducts()
+      .then((response) => {
+        this.products = response.data;
+      })
+      .catch((err) => console.error(err));
   },
 };
 </script>
