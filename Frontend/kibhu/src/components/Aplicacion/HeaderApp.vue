62b42ccb1cada8a43c3b36f67a168ee1ca340101
Acosticahub <acostabuitrago0123@gmail.com>
2021-10-12 09:43:53 -0500

    Boton Salir
    Header

--- Frontend/kibhu/src/components/Aplicacion/HeaderApp.vue
+++ Frontend/kibhu/src/components/Aplicacion/HeaderApp.vue
@@ -1,17 +1,20 @@
 <template>
-<nav>
-<v-app-bar flat app >
-    <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
-    <v-img
-    contain
-    lazy-src="../../assets/Logo Kibhu blanco-02.svg"
-    max-height="200"
-    max-width="130"
-    src="../../assets/Logo Kibhu Para fondo claro -02.png"
-></v-img>
-<v-spacer></v-spacer>
+  <nav>
+    <v-app-bar flat app>
+      <v-app-bar-nav-icon
+        class="grey--text"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-img
+        contain
+        lazy-src="../../assets/Logo Kibhu blanco-02.svg"
+        max-height="200"
+        max-width="130"
+        src="../../assets/Logo Kibhu Para fondo claro -02.png"
+      ></v-img>
+      <v-spacer></v-spacer>
 
-   <!-- <div class="text-center">
+      <!-- <div class="text-center">
     <v-menu offset-y>
       <template v-slot:activator="{ on, attrs }">
         <v-btn
@@ -35,92 +38,101 @@
     </v-menu>
   </div>-->
 
-  <div class="dropdown">
-  
-  <button class="dropbtn">Menu <v-icon left>mdi-chevron-down</v-icon> </button>
-  <div class="dropdown-content">
-  <a style="black" href="/Appkibhu">Inicio</a>
-  <a href="/productos">Productos</a>
-  <a href="/usuarios">Usuarios</a>
-  <a href="/Clientes">Clientes</a>
-  <a href="/Proveedores">Proveedores</a>
-  </div>
-</div>
+      <div class="dropdown">
+        <button class="dropbtn">
+          Menu <v-icon left>mdi-chevron-down</v-icon>
+        </button>
+        <div class="dropdown-content">
+          <a style="black" href="/Appkibhu">Inicio</a>
+          <a href="/productos">Productos</a>
+          <a href="/usuarios">Usuarios</a>
+          <a href="/Clientes">Clientes</a>
+          <a href="/Proveedores">Proveedores</a>
+        </div>
+      </div>
 
-
-<v-btn class="sign" flat link href="../../" >
-    <span>Sign Out</span>
-    <v-icon rigth>mdi-logout</v-icon>
-</v-btn>
-</v-app-bar>
-<v-navigation-drawer app v-model="drawer" color="#dAA520">
-  <v-row  class="mt-5 mb-3">
+      <v-btn class="sign" flat link href="../../">
+        <span>Salir</span>
+        <v-icon rigth>mdi-logout</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-navigation-drawer app v-model="drawer" color="#dAA520">
+      <v-row class="mt-5 mb-3">
         <v-col cols="7">
-            <v-avatar size="160" class="ml-5">
-                <img src="../../assets/Admin-01.png" alt="."/>
-            </v-avatar>
-            
-            </v-col>
-        </v-row>
-        <v-btn class="btnadmin">
-    Administrador 
-    </v-btn>
-    <p class="text-center white--text font-weight-medium "> Hernán Pérez </p>
-    <v-list dense nav>
-        
-        <v-list-item class="white--text" v-for="link in links" :key="link.text" route :to="link.route">
-            <v-list-item-action>
-                <v-icon class="white--text"> {{ link.icon }} </v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-                <v-list-item-item class="white--text"> {{link.text}} </v-list-item-item>
-            </v-list-item-content>
+          <v-avatar size="160" class="ml-5">
+            <img src="../../assets/Admin-01.png" alt="." />
+          </v-avatar>
+        </v-col>
+      </v-row>
+      <v-btn class="btnadmin"> Administrador </v-btn>
+      <p class="text-center white--text font-weight-medium">Hernán Pérez</p>
+      <v-list dense nav>
+        <v-list-item
+          class="white--text"
+          v-for="link in links"
+          :key="link.text"
+          route
+          :to="link.route"
+        >
+          <v-list-item-action>
+            <v-icon class="white--text"> {{ link.icon }} </v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-item class="white--text">
+              {{ link.text }}
+            </v-list-item-item>
+          </v-list-item-content>
         </v-list-item>
-    </v-list>
-</v-navigation-drawer>
-</nav>
-
-
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
-
-
 export default {
-    data () {
-        return {
-            drawer: false,
-        
-            links:[
-                {icon:'mdi-view-dashboard', text: 'Configuración de Usuario', route: ''},
-                { icon: 'mdi-folder-settings', text: 'Recursos', route: './Proyects'},
-                {icon: 'mdi-account-tie', text: 'Equipo', route:'./Usuarios'},
-            ],
-            items:[
-                {icon: 'mdi-account-tie', text: 'Inicio', route:'./'},
-                {icon:'mdi-view-dashboard', text: 'Productoss', route: './Productos'},
-                {icon: 'mdi-folder-settings', text: 'Clientes', route: './Clientes'},
-                {icon: 'mdi-account-tie', text: 'Proveedores', route:'./Proveedores'},
-                {icon: 'mdi-account-tie', text: 'Usuarios', route:'./Usuarios'},
-            ],
-            
-            
-        }
-        }
-}
+  data() {
+    return {
+      drawer: false,
 
+      links: [
+        {
+          icon: "mdi-view-dashboard",
+          text: "Configuración de Usuario",
+          route: "",
+        },
+        { icon: "mdi-folder-settings", text: "Recursos", route: "./Proyects" },
+        { icon: "mdi-account-tie", text: "Equipo", route: "./Usuarios" },
+      ],
+      items: [
+        { icon: "mdi-account-tie", text: "Inicio", route: "./" },
+        {
+          icon: "mdi-view-dashboard",
+          text: "Productoss",
+          route: "./Productos",
+        },
+        { icon: "mdi-folder-settings", text: "Clientes", route: "./Clientes" },
+        {
+          icon: "mdi-account-tie",
+          text: "Proveedores",
+          route: "./Proveedores",
+        },
+        { icon: "mdi-account-tie", text: "Usuarios", route: "./Usuarios" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-.sign{
-  
-    text-decoration: none !important;
+.sign {
+  text-decoration: none !important;
+  margin-left: 2%;
 }
-.foto{
+.foto {
   left: 50px;
 }
 
-.btnadmin{
+.btnadmin {
   bottom: 15px;
   left: 50px;
 }
@@ -143,7 +155,7 @@ export default {
   position: absolute;
   background-color: #f0e9e9;
   min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   z-index: 1;
 }
 
@@ -154,7 +166,9 @@ export default {
   display: block;
 }
 
-.dropdown-content a:hover {background-color: #f1f1f1}
+.dropdown-content a:hover {
+  background-color: #f1f1f1;
+}
 
 .dropdown:hover .dropdown-content {
   display: block;
@@ -163,5 +177,4 @@ export default {
 .dropdown:hover .dropbtn {
   background-color: #ffffff;
 }
-
 </style>
